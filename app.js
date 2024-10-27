c617da71e5f72a05b295fe6e36f051d746de6d97
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2024-10-27 23:48:20 +0530

    copied text alert added

--- app.js
+++ app.js
@@ -87,9 +87,11 @@ function buttonClick(btn) {
   }, 400);
 }
 
+
+
 var encryptedString = ""
 function encryptString() {
-
+  encryptedString = ""
   const text = document.getElementById("textToEncrypt").value
   const originalString = text
 
@@ -102,7 +104,7 @@ function encryptString() {
     document.getElementById("outputArea").innerText = encryptedString
     console.log(encryptedString)
   }
-  else{
+  else {
     document.getElementById("textToEncrypt").placeholder = "Enter some text first!!!"
   }
 
@@ -126,4 +128,41 @@ function decryptString() {
   }
   console.log(decryptedString)
 
+}
+
+alertShown = false
+function copyToClipboard() {
+
+  navigator.clipboard.writeText(encryptedString).then(
+    (e) => {
+      if (!alertShown) {
+        document.getElementById("alertContaingBox").insertAdjacentHTML("beforeend",
+          `
+              <div class="alertCardTextCopied d-flex px-3 py-2 rounded-3 align-items-center ">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" fill="currentColor"
+                  class="bi bi-check-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path
+                      d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
+              </svg>
+  
+              <text class="mx-1">Text copied to clipboard</text>
+          </div>
+          `
+        )
+        alertShown = true
+        setTimeout(() => {
+          document.getElementById("alertContaingBox").innerHTML = ""
+          alertShown = false
+
+        }, 3000);
+      }
+
+
+
+    },
+    (re) => {
+      console.log(re)
+    },
+  );
 }
