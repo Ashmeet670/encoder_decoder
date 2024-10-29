cc150341b130526bf58a3fc4707d03b0189dc580
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2024-10-29 23:57:41 +0530

    added exit animation

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const lowercaseAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 const uppercaseAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -138,7 +139,7 @@ function copyToClipboard() {
       if (!alertShown) {
         document.getElementById("alertContaingBox").insertAdjacentHTML("beforeend",
           `
-              <div class="alertCardTextCopied d-flex px-3 py-2 rounded-3 align-items-center ">
+              <div id="alertCopy"  class="alertCardTextCopied d-flex px-3 py-2 rounded-3 align-items-center ">
               <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" fill="currentColor"
                   class="bi bi-check-circle" viewBox="0 0 16 16">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
@@ -152,10 +153,32 @@ function copyToClipboard() {
         )
         alertShown = true
         setTimeout(() => {
-          document.getElementById("alertContaingBox").innerHTML = ""
+          document.getElementById("alertCopy").classList.add("exitAnimation")
           alertShown = false
+          setTimeout(() => {
+            document.getElementById("alertContaingBox").innerHTML = ""
+          }, 450);
 
         }, 3000);
+      }
+      else {
+        document.getElementById("alertContaingBox").innerHTML = ""
+        document.getElementById("alertContaingBox").insertAdjacentHTML("beforeend",
+          `
+            <div id="alertCopy"  class="alertCardTextCopied d-flex px-3 py-2 rounded-3 align-items-center ">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" fill="currentColor"
+                  class="bi bi-check-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path
+                      d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
+              </svg>
+  
+              <text class="mx-1">Text copied to clipboard</text>
+          </div>
+          `
+        )
+        alertShown = true
+
       }
 
 
